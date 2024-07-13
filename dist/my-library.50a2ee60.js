ea1f8e6e799a8943d28d8f415ba378d4643306fc
rudopulos <79414851+rudopulos@users.noreply.github.com>
2024-07-13 17:46:11 +0300

    V5

--- dist/my-library.50a2ee60.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};e.closeModalBtn.addEventListener("click",(function(){e.modal.classList.toggle("gallery-is-hidden")}))}();
-//# sourceMappingURL=my-library.50a2ee60.js.map
